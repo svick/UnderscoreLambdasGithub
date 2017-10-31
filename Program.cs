a960d08e8ac2a80cafd54f206e9d5e454fd2358c
Petr Onderka <gsvick@gmail.com>
2017-10-31 20:22:46 +0100

    Stuff, I guess?

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Microsoft.CodeAnalysis;
@@ -16,17 +18,17 @@ namespace UnderscoreLambdasGithub
 {
     public class Program
     {
-        //static readonly Dictionary<string, int> underscoreRepos = new Dictionary<string, int>();
-
         public static void Main()
         {
-            //Repos = GetRepos().ToArray();
+            Repos = GetRepos(10).Distinct().ToArray();
+
+            Console.WriteLine($"{Repos.Length} repos");
 
             var commonOptions = new ExecutionDataflowBlockOptions { BoundedCapacity = 1 };
             var parallelOptions = new ExecutionDataflowBlockOptions
             {
-                BoundedCapacity = 2,
-                MaxDegreeOfParallelism = 2,
+                BoundedCapacity = 4,
+                MaxDegreeOfParallelism = 4,
                 EnsureOrdered = false
             };
 
@@ -36,10 +38,10 @@ namespace UnderscoreLambdasGithub
 
             getFilesBlock.SendAsync(BasePath).Wait();
 
-            var parseBlock = new TransformBlock<string, LambdaData>(file => Parse(file), commonOptions);
+            var parseBlock = new TransformBlock<string, Data>(file => Parse(file), commonOptions);
 
-            LambdaData lambdaDataSummary = new LambdaData();
-            var summarizeBlock = new ActionBlock<LambdaData>(ld => lambdaDataSummary.Add(ld));
+            Data dataSummary = new Data();
+            var summarizeBlock = new ActionBlock<Data>(ld => dataSummary.Add(ld));
 
             var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
 
@@ -53,40 +55,44 @@ namespace UnderscoreLambdasGithub
 
             sendDataTask.Wait();
 
-            Print(lambdaDataSummary);
+            Print(dataSummary);
 
             Console.WriteLine();
-
-            //Print(underscoreRepos);
         }
 
-        private static void Print(LambdaData lambdaData)
+        private static void Print(Data data)
         {
-            Console.WriteLine($"single lambdas: {lambdaData.TotalSingleLambdasCount}, multi lambdas: {lambdaData.TotalMultiLambdasCount}");
-
-            Console.WriteLine("Single lambdas:");
-            Print(lambdaData.SingleLambdas);
-
-            Console.WriteLine("Multi lambdas, one parameter:");
-            Print(lambdaData.MultiLambdasOneParameter);
-
-            Console.WriteLine("Multi lambdas, multi parameters:");
-            Print(lambdaData.MultiLambdasMultiParameters);
+            Print(data.Joins);
         }
 
         private static void Print(Dictionary<string, int> dictionary)
         {
-            Console.WriteLine($"Total: {dictionary.Values.Sum()}");
-
             var data = dictionary.OrderByDescending(kvp => kvp.Value).Take(10);
+
+            Console.WriteLine($"Total: {dictionary.Values.Sum()}");
 
             foreach (var kvp in data)
             {
-                Console.WriteLine($"{kvp.Key}\t{kvp.Value}");
+                Console.WriteLine($"'{kvp.Key}'\t{kvp.Value}");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("| | count |");
+            Console.WriteLine("|---|---|");
+
+            Console.WriteLine($"| total | {dictionary.Values.Sum()} |");
+
+            foreach (var kvp in data)
+            {
+                Console.WriteLine($"| \"{Format(kvp.Key)}\" | {kvp.Value} |");
             }
         }
 
-        private static readonly string BasePath = @"C:\Users\Svick\AppData\Local\Temp\UnderscoreLambdasGithub\";
+        private static string Format(string s) => s.Replace("\n", "\\n").Replace("\r", "\\r").Replace("\t", "\\t");
+
+        //private static readonly string BasePath = @"C:\Users\Svick\AppData\Local\Temp\UnderscoreLambdasGithub\";
+        private static readonly string BasePath = @"E:\Temp\UnderscoreLambdasGithub\";
 
         private static async Task SendData(ITargetBlock<string> targetBlock)
         {
@@ -98,143 +104,103 @@ namespace UnderscoreLambdasGithub
             targetBlock.Complete();
         }
 
-        private static LambdaData Parse(string file)
+        private static Data Parse(string file)
         {
-            Console.WriteLine($"Parsing {file.Substring(BasePath.Length)}.");
+            //Console.WriteLine($"Parsing {file.Substring(BasePath.Length)}.");
 
-            var lambdaData = new LambdaData();
+            var data = new Data();
 
             var syntaxTree = CSharpSyntaxTree.ParseText(SourceText.From(File.OpenRead(file)));
-            var semanticModel = CSharpCompilation.Create(null).AddSyntaxTrees(syntaxTree).GetSemanticModel(syntaxTree);
+            var semanticModel = CSharpCompilation.Create(null)
+                .AddSyntaxTrees(syntaxTree)
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location))
+                .GetSemanticModel(syntaxTree);
 
-            var lambdas = syntaxTree.GetCompilationUnitRoot().DescendantNodes().OfType<LambdaExpressionSyntax>();
+            var invocations = syntaxTree.GetCompilationUnitRoot().DescendantNodes().OfType<InvocationExpressionSyntax>();
 
-            foreach (var lambda in lambdas)
+            foreach (var invocation in invocations)
             {
-                var names = lambda.DescendantNodes().OfType<NameSyntax>().Select(name => semanticModel.GetSymbolInfo(name).Symbol).Where(s => s != null).ToList();
-
-                var simpleLambda = lambda as SimpleLambdaExpressionSyntax;
-                if (simpleLambda != null)
+                if (invocation.ToString().Contains("Join"))
                 {
-                    var paramaterSymbol = semanticModel.GetDeclaredSymbol(simpleLambda.Parameter);
-
-                    if (SingleLambda(lambdaData, names, paramaterSymbol))
-                    {
-                        //LambdaData.Increment(underscoreRepos, file.Substring(BasePath.Length).Split('\\')[0]);
-                    }
+                    //Console.WriteLine(file.Substring(BasePath.Length) + ':');
+                    Console.WriteLine(invocation);
                 }
 
-                var complexLambda = lambda as ParenthesizedLambdaExpressionSyntax;
-                if (complexLambda != null)
-                {
-                    var parameterSymbols = complexLambda.ParameterList.Parameters
-                        .Select(param => semanticModel.GetDeclaredSymbol(param))
-                        .ToList();
+                var symbolInfo = semanticModel.GetSymbolInfo(invocation.Expression);
+                IEnumerable<ISymbol> symbols = symbolInfo.CandidateSymbols;
+                if (symbolInfo.Symbol != null)
+                    symbols = symbols.Prepend(symbolInfo.Symbol);
 
-                    if (parameterSymbols.Count == 1)
-                    {
-                        if (SingleLambda(lambdaData, names, parameterSymbols.Single()))
-                        {
-                            //LambdaData.Increment(underscoreRepos, file.Substring(BasePath.Length).Split('\\')[0]);
-                        }
-                    }
-                    else
-                    {
-                        MultiLambda(lambdaData, names, parameterSymbols);
-                    }
-                }
+                if (!symbols.Any(s => s.ContainingType?.SpecialType == SpecialType.System_String && s.Name == "Join"))
+                    continue;
+
+                var firstParameterLiteral = invocation.ArgumentList.Arguments[0].Expression as LiteralExpressionSyntax;
+
+                if (firstParameterLiteral == null)
+                    continue;
+
+                var firstParameterValue = firstParameterLiteral.Token.ValueText;
+
+                Console.WriteLine($"Found '{firstParameterValue}'");
+                data.Join(firstParameterValue);
             }
 
-            return lambdaData;
-        }
-
-        private static void MultiLambda(LambdaData lambdaData, List<ISymbol> names, List<IParameterSymbol> parameterSymbols)
-        {
-            lambdaData.MultiLambda();
-
-            var missingSymbols =
-                (from parameterSymbol in parameterSymbols
-                 where !names.Contains(parameterSymbol)
-                 select parameterSymbol.Name).ToList();
-
-            switch (missingSymbols.Count)
-            {
-                case 0:
-                    break;
-                case 1:
-                    lambdaData.MultiLambdaOneUnused(missingSymbols.Single());
-                    break;
-                default:
-                    lambdaData.MultiLambdaMultiUnused(string.Join(", ", missingSymbols));
-                    break;
-            }
-        }
-
-        private static bool SingleLambda(LambdaData lambdaData, List<ISymbol> names, IParameterSymbol parameterSymbol)
-        {
-            lambdaData.SingleLambda();
-
-            if (!names.Contains(parameterSymbol))
-            {
-                lambdaData.SingleLambdaUnused(parameterSymbol.Name);
-
-                if (parameterSymbol.Name == "_")
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return data;
         }
 
         private static IEnumerable<string> GetFiles(string path)
-            => Directory.EnumerateFiles(path, "*.cs", SearchOption.AllDirectories);
+        {
+            // this can happen when clone fails
+            if (!Directory.Exists(path))
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            return Directory.EnumerateFiles(path, "*.cs", SearchOption.AllDirectories);
+        }
+
+        private static int i;
 
         private static string Clone(string repo)
         {
-            Console.WriteLine(repo);
+            Console.WriteLine($"{Interlocked.Increment(ref i)} {repo}");
 
             string gitUrl = $"https://github.com/{repo}.git";
-            string path = Path.Combine(BasePath, repo.Split('/')[1]);
+            string path = Path.Combine(BasePath, repo.Replace('/', ' '));
 
             if (Directory.Exists(path))
                 Console.WriteLine($"Directory for {repo} already exists.");
             else
-                Process.Start("git", $"clone {gitUrl} {path} --depth 1").WaitForExit();
+                Process.Start("git", $"clone {gitUrl} \"{path}\" --depth 1").WaitForExit();
 
             return path;
         }
 
         private static string[] Repos;
 
-        static IEnumerable<string> GetRepos()
+        static IEnumerable<string> GetRepos(int pages)
         {
             var client = new HttpClient();
 
             client.DefaultRequestHeaders.UserAgent.ParseAdd("gsvick at gmail.com");
 
-            var jsonString = client.GetStringAsync(
-                "https://api.github.com/search/repositories?q=language:csharp+stars:%3C=100&sort=updated&per_page=100").Result;
-
-            dynamic data = JsonConvert.DeserializeObject(jsonString);
-            foreach (var repo in data.items)
+            for (int i = 1; i <= pages; i++)
             {
-                yield return repo.full_name;
+                var jsonString = client.GetStringAsync(
+                    $"https://api.github.com/search/repositories?q=language:csharp+stars:%3C=100&sort=updated&per_page=100&page={i}").Result;
+
+                dynamic data = JsonConvert.DeserializeObject(jsonString);
+                foreach (var repo in data.items)
+                {
+                    yield return repo.full_name;
+                }
             }
         }
     }
 
-    class LambdaData
+    class Data
     {
-        public int TotalSingleLambdasCount { get; private set; }
-        public int TotalMultiLambdasCount { get; private set; }
-
-        public Dictionary<string, int> SingleLambdas { get; } = new Dictionary<string, int>();
-        public Dictionary<string, int> MultiLambdasOneParameter { get; } = new Dictionary<string, int>();
-        public Dictionary<string, int> MultiLambdasMultiParameters { get; } = new Dictionary<string, int>();
-
-        public void SingleLambda() => TotalSingleLambdasCount++;
-        public void MultiLambda() => TotalMultiLambdasCount++;
+        public Dictionary<string, int> Joins { get; } = new Dictionary<string, int>();
 
         public static void Increment<T>(Dictionary<T, int> dictionary, T key, int added = 1)
         {
@@ -243,9 +209,7 @@ namespace UnderscoreLambdasGithub
             dictionary[key] = value + added;
         }
 
-        public void SingleLambdaUnused(string name) => Increment(SingleLambdas, name);
-        public void MultiLambdaOneUnused(string name) => Increment(MultiLambdasOneParameter, name);
-        public void MultiLambdaMultiUnused(string name) => Increment(MultiLambdasMultiParameters, name);
+        public void Join(string separator) => Increment(Joins, separator);
 
         private static void Add(Dictionary<string, int> thisDictionary, Dictionary<string, int> otherDictionary)
         {
@@ -255,14 +219,9 @@ namespace UnderscoreLambdasGithub
             }
         }
 
-        public void Add(LambdaData other)
+        public void Add(Data other)
         {
-            this.TotalSingleLambdasCount += other.TotalSingleLambdasCount;
-            this.TotalMultiLambdasCount += other.TotalMultiLambdasCount;
-
-            Add(this.SingleLambdas, other.SingleLambdas);
-            Add(this.MultiLambdasOneParameter, other.MultiLambdasOneParameter);
-            Add(this.MultiLambdasMultiParameters, other.MultiLambdasMultiParameters);
+            Add(this.Joins, other.Joins);
         }
     }
 }
